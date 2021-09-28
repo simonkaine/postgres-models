62892d4ad6858db7cdbfd8ff3c40b8e80fd80dbf
Simon Kaine <simonkaine@gmail.com>
2021-09-27 17:04:29 -0700

    test for GET munged api data + Passing test.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,10 +2,21 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-
+// 'Rick Sanchez' 'Alive' 'Human'
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('gets munged api data', () => {
+    return request(app).get('/api/character').then((res) => {
+
+      expect(res.body).toEqual({
+        'name': expect.any(String),
+        'status': expect.any(String),
+        'species': expect.any(String)
+      });
+    });
   });
 
   afterAll(() => {
