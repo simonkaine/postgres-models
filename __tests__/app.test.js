180428336d667972058fd70f38d30168e0b5d27e
Simon Kaine <simonkaine@gmail.com>
2021-09-28 18:10:55 -0700

    two passing tests for GET all and GET by Id + POST in progress.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,38 +1,71 @@
+/* eslint-disable indent */
 import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 // 'Rick Sanchez' 'Alive' 'Human'
+
 describe('demo routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('gets munged api data', () => {
-    return request(app).get('/api/character').then((res) => {
+  // it('gets munged api data', () => {
+  //   return request(app).get('/api/character').then((res) => {
+  //     expect(res.body).toEqual({
+  //       id: '1',
+  //       name: 'Rick Sanchez', 
+  //       status: 'Alive', 
+  //       species: 'Human'
+  //     });
+  //   });
+  // });
 
-      expect(res.body).toEqual({
-        'name': expect.any(String),
-        'status': expect.any(String),
-        'species': expect.any(String)
-      });
+  it('should GET all data back from the database', async () => {
+    await request(app).post('/api/character').send(); 
+    return request(app)
+    .get('/api/character').then((res) => {
+  
+      expect(res.body).toEqual(expect.arrayContaining([expect.objectContaining( 
+        { 
+          'name': expect.any(String),
+          'status': expect.any(String),
+          'species': expect.any(String)
+        }
+      )]));
     });
   });
 
-  it('creates/POST a new character in our database', () => {
+  // it('creates/POST to our database', async () => {
+  //   await request(app)
+  //     .post('/api/character').send()
+  //     .then(res => {
+  //       console.log('!!!!!!!!!!!!', res.body);
+  //       expect(res.body).toEqual(expect.arrayContaining([expect.objectContaining( 
+  //         { 
+  //           'name': expect.any(String),
+  //           'status': expect.any(String),
+  //           'species': expect.any(String)
+  //         }
+  //       )]));
+  //     });
+  // });
+
+  it('should GET newly created character by ID', async () => {
+    await request(app).post('/api/character').send({
+          id: '1', 
+          name: 'Rick Sanchez', 
+          status: 'Alive', 
+          species: 'Human',
+    }); 
     return request(app)
-      .post('/api/character')
-      .send({     
-        'name': 'Simon Kaine',
-        'status': 'Alive',
-        'species': 'Alien Beast and Lord Of All Space and Time', })
-      .then(res => {
-        console.log('POST TEST RESPONSE BODY', res.body);
-        expect(res.body).toEqual({
-          'id': '1', // Should i make this one?
-          'name': 'Simon Kaine',
-          'status': 'Alive',
-          'species': 'Alien Beast and Lord Of All Space and Time',
+      .get('/api/character/1').then(res => {
+  
+        expect(res.body).toEqual({ // res.body[0] CORRECT<<<<<<< ???
+          id: '1', 
+          name: 'Rick Sanchez', 
+          status: 'Alive', 
+          species: 'Human',
         });
       });
   });
